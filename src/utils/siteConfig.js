6bc2e586c514bfd861c263b2b8fc2931a2029d98
b0ss-k1ng <41974428+b0ss-k1ng@users.noreply.github.com>
2021-09-18 14:07:18 +0100

    Update siteConfig.js
    finally added the correct link 🥲

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `festive-allen-92f455.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://ghost-sparetimr-v1.herokuapp.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
